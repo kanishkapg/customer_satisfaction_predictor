44c339a522ab8fcda49058e8282a7094cc1d95b8
Kanishka Gunasinghe <kanishkapg1121@gmail.com>
2025-03-23 12:37:00 +0530

    Implemented data cleaning

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,4 +2,4 @@ from pipelines.training_pipeline import train_pipeline
 
 if __name__ == "__main__":
     # Run the pipeline
-    train_pipeline(data_path='G:\\Sales Predictor\\data\\olist_customers_dataset.csv')
+    train_pipeline(data_path='G:\Customer Satisfaction Pred\customer_satisfaction_predictor\data\olist_customers_dataset.csv')
--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,21 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataPreProcessStrategy, DataDivideStrategy, DataCleaning
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
 def clean_df(df: pd.DataFrame) -> None:
-    pass
+    try:
+        process_startegy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_startegy)
+        processed_data = data_cleaning.handle_data()
+        
+        divide_startegy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_startegy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed successfully")
+    except Exception as e:
+        logging.error("Error in cleaning data: {}".format(e))
+        raise e
